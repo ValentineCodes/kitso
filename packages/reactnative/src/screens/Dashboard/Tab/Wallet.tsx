cb4d350839b5619abf82dbc3b96463dcd3ee1cef
Valentine Orga <valentineorga@gmail.com>
2024-08-30 11:20:46 +0100

    feat: display user balance

--- packages/reactnative/src/screens/Dashboard/Tab/Wallet.tsx
+++ packages/reactnative/src/screens/Dashboard/Tab/Wallet.tsx
@@ -15,6 +15,7 @@ import CopyableText from '../../../components/CopyableText'
 import useAccount from '../../../hooks/scaffold-eth/useAccount'
 import useNetwork from '../../../hooks/scaffold-eth/useNetwork'
 import { useProfile } from '../../../context/UniversalProfileContext'
+import Token from '../../../components/Token'
 
 let backHandler: NativeEventSubscription;
 
@@ -22,78 +23,6 @@ type WalletProps = {}
 type LinkProps = {
     title: string
     url: string
-}
-
-function Token() {
-    const network = useNetwork()
-
-    return (
-        <VStack
-            borderWidth={"1"}
-            borderRadius={"2xl"}
-            borderColor={"gray.200"}
-            pt={"8"}
-            pb={"4"}
-            pl={"6"}
-            pr={"4"}
-        >
-            <HStack
-                space={"6"}
-            >
-                <View
-                    borderWidth={1}
-                    borderRadius={"full"}
-                    borderColor={"gray.200"}
-                    w={WINDOW_WIDTH * 0.17}
-                    h={WINDOW_WIDTH * 0.17}
-                    p={3}
-                >
-                    <Image
-                        source={require("../../../../assets/images/lukso_logo.png")}
-                        alt="LYX token"
-                        w={"full"}
-                        h={"full"}
-                    />
-                </View>
-
-                <VStack space={0.2}>
-                    <Text
-                        fontSize={"sm"}
-                        bold
-                    >
-                        {network.name.toUpperCase()}
-                    </Text>
-
-                    <HStack alignItems={"center"} space={2}>
-                        <Text fontSize={"2xl"} bold>0</Text>
-                        <Text fontSize={"md"} bold color={"gray.400"}>{network.token}</Text>
-                    </HStack>
-                    <Text
-                        fontSize={"sm"}
-                        fontWeight={"medium"}
-                        color={"gray.600"}
-                    >
-                        $0.00
-                    </Text>
-                </VStack>
-            </HStack>
-
-            <Pressable>
-                <Text
-                    alignSelf={"flex-end"}
-                    mt={3}
-                    px={"4"}
-                    py={"1"}
-                    borderWidth={"1"}
-                    borderRadius={"2xl"}
-                    borderColor={"gray.300"}
-                    fontWeight={"medium"}
-                >
-                    Buy
-                </Text>
-            </Pressable>
-        </VStack>
-    )
 }
 
 function Link({ title, url }: LinkProps) {
@@ -153,8 +82,6 @@ function Wallet({ }: WalletProps) {
     }, [])
 
     if (!isFocused) return
-
-    console.log(profile?.backgroundImage)
 
     return (
         <ScrollView contentContainerStyle={{ flexGrow: 1, paddingBottom: 20 }} bgColor={"white"}>
